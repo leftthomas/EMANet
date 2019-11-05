e82b9ba15ed4e264f12f9c3e9b198fed67e8a8fa
leftthomas <leftthomas@qq.com>
2019-11-05 15:16:56 +0800

    finish DilatedResNet

--- ema/backbone.py
+++ ema/backbone.py
@@ -1,7 +1,5 @@
 import fvcore.nn.weight_init as weight_init
-import numpy as np
-import torch.nn.functional as F
-from detectron2.layers import Conv2d, FrozenBatchNorm2d, ShapeSpec
+from detectron2.layers import Conv2d
 from detectron2.modeling.backbone import Backbone
 from detectron2.modeling.backbone.build import BACKBONE_REGISTRY
 from torch import nn
@@ -9,200 +7,148 @@ from torch import nn
 from .arch import get_norm
 
 
-class BasicStem(nn.Module):
-    def __init__(self, in_channels=3, out_channels=64, norm="BN", bn_mom=0.1):
-        """
-        Args:
-            norm (str or callable): a callable that takes the number of
-                channels and return a `nn.Module`, or a pre-defined string
-                (one of {"BN", "SyncBN"}).
-        """
-        super().__init__()
+class Bottleneck(nn.Module):
+    """ResNet Bottleneck
+    Parameters
+    ----------
+    norm_layer (str or callable): a callable that takes the number of
+        channels and return a `nn.Module`, or a pre-defined string
+        (one of {"BN", "SyncBN"}).
+    """
+    expansion = 4
 
-        self.out_channels = out_channels
-
-        self.conv1 = nn.Sequential(
-            Conv2d(in_channels, out_channels // 2, kernel_size=3, stride=2, padding=1, bias=False,
-                   norm=get_norm(norm, out_channels // 2, bn_mom)),
-            nn.ReLU(inplace=True),
-            Conv2d(out_channels // 2, out_channels // 2, kernel_size=3, stride=1, padding=1, bias=False,
-                   norm=get_norm(norm, out_channels // 2, bn_mom)),
-            nn.ReLU(inplace=True),
-            Conv2d(out_channels // 2, out_channels, kernel_size=3, stride=1, padding=1, bias=False,
-                   norm=get_norm(norm, out_channels, bn_mom)),
-        )
-
-        for module in self.modules():
-            if isinstance(module, Conv2d):
-                weight_init.c2_msra_fill(module)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu_(x)
-        x = F.max_pool2d(x, kernel_size=3, stride=2, padding=1)
-        return x
-
-    @property
-    def stride(self):
-        return 4  # = stride 2 conv -> stride 2 max pool
-
-
-class ResNetBlockBase(nn.Module):
-    def __init__(self, in_channels, out_channels, stride):
-        """
-        The `__init__` method of any subclass should also contain these arguments.
-        Args:
-            in_channels (int):
-            out_channels (int):
-            stride (int):
-        """
-        super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
+    def __init__(self, inplanes, planes, stride=1, dilation=1, downsample=None, norm_layer='BN', bn_mom=0.1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = get_norm(norm_layer, planes, bn_mom)
+        self.conv2 = Conv2d(planes, planes, kernel_size=3, stride=stride, padding=dilation, dilation=dilation,
+                            bias=False)
+        self.bn2 = get_norm(norm_layer, planes, bn_mom)
+        self.conv3 = Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = get_norm(norm_layer, planes * 4, bn_mom)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.dilation = dilation
         self.stride = stride
 
-    def freeze(self):
-        for p in self.parameters():
-            p.requires_grad = False
-        FrozenBatchNorm2d.convert_frozen_batchnorm(self)
-        return self
-
-
-class BottleneckBlock(ResNetBlockBase):
-    def __init__(self, inplanes, planes, stride=1, norm="BN", bn_mom=0.1, dilation=1):
-        """
-        Args:
-            norm (str or callable): a callable that takes the number of
-                channels and return a `nn.Module`, or a pre-defined string
-                (one of {"BN", "SyncBN"}).
-        """
-        super().__init__(inplanes, planes * 4, stride)
-
-        if stride != 1 or inplanes != planes * 4:
-            self.shortcut = Conv2d(inplanes, planes * 4, kernel_size=1, stride=stride, bias=False,
-                                   norm=get_norm(norm, planes * 4, bn_mom))
-        else:
-            self.shortcut = None
-
-        self.conv1 = Conv2d(inplanes, planes, kernel_size=1, bias=False, norm=get_norm(norm, planes, bn_mom))
-
-        self.conv2 = Conv2d(planes, planes, kernel_size=3, stride=stride, padding=dilation, bias=False,
-                            dilation=dilation, norm=get_norm(norm, planes, bn_mom))
-
-        self.conv3 = Conv2d(planes, planes * 4, kernel_size=1, bias=False, norm=get_norm(norm, planes * 4, bn_mom))
-
-        for layer in [self.conv1, self.conv2, self.conv3, self.shortcut]:
-            if layer is not None:  # shortcut can be None
-                weight_init.c2_msra_fill(layer)
-
     def forward(self, x):
+        residual = x
+
         out = self.conv1(x)
-        out = F.relu_(out)
+        out = self.bn1(out)
+        out = self.relu(out)
 
         out = self.conv2(out)
-        out = F.relu_(out)
+        out = self.bn2(out)
+        out = self.relu(out)
 
         out = self.conv3(out)
+        out = self.bn3(out)
 
-        if self.shortcut is not None:
-            shortcut = self.shortcut(x)
-        else:
-            shortcut = x
+        if self.downsample is not None:
+            residual = self.downsample(x)
 
-        out += shortcut
-        out = F.relu_(out)
+        out += residual
+        out = self.relu(out)
+
         return out
 
 
-def make_stage(inplanes, planes, blocks, stride=1, dilation=1, norm="BN", bn_mom=0.1, grids=None):
-    layers = []
-    if grids is None:
-        grids = [1] * blocks
-
-    if dilation == 1 or dilation == 2:
-        layers.append(BottleneckBlock(inplanes, planes, stride, norm=norm, bn_mom=bn_mom, dilation=1))
-    elif dilation == 4:
-        layers.append(BottleneckBlock(inplanes, planes, stride, norm=norm, bn_mom=bn_mom, dilation=2))
-    else:
-        raise RuntimeError('=> unknown dilation size: {}'.format(dilation))
-
-    for i in range(1, blocks):
-        layers.append(BottleneckBlock(planes * 4, planes, norm=norm, bn_mom=bn_mom, dilation=dilation * grids[i]))
-
-    return layers
-
-
 class DilatedResNet(Backbone):
-    def __init__(self, stem, stages, num_classes=None, out_features=None):
-        """
-        Args:
-            stem (nn.Module): a stem module
-            stages (list[list[ResNetBlock]]): several (typically 4) stages,
-                each contains multiple :class:`ResNetBlockBase`.
-            num_classes (None or int): if None, will not perform classification.
-            out_features (list[str]): name of the layers whose outputs should
-                be returned in forward. Can be anything in "stem", "linear", or "res2" ...
-                If None, will return the output of the last layer.
-        """
+    """Dilated Pre-trained ResNet Model, which preduces the stride of 8 featuremaps at conv5.
+    Parameters
+    ----------
+    layers : list of int
+        Numbers of layers in each block
+    norm_layer (str or callable): a callable that takes the number of
+        channels and return a `nn.Module`, or a pre-defined string
+        (one of {"BN", "SyncBN"}).
+    """
+
+    def __init__(self, layers, stride=8, norm_layer='BN', bn_mom=0.1):
+        self.inplanes = 128
+        assert stride in [8, 16], RuntimeError("=> unknown stride size: {}".format(stride))
         super(DilatedResNet, self).__init__()
-        self.stem = stem
-        self.num_classes = num_classes
+        self.conv1 = nn.Sequential(
+            Conv2d(3, 64, kernel_size=3, stride=2, padding=1, bias=False),
+            get_norm(norm_layer, 64, bn_mom),
+            nn.ReLU(inplace=True),
+            Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            get_norm(norm_layer, 64, bn_mom),
+            nn.ReLU(inplace=True),
+            Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=False),
+        )
+        self.bn1 = get_norm(norm_layer, self.inplanes, bn_mom)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(64, layers[0], norm_layer=norm_layer, bn_mom=bn_mom)
+        self.layer2 = self._make_layer(128, layers[1], stride=2, norm_layer=norm_layer, bn_mom=bn_mom)
+        if stride == 8:
+            self.layer3 = self._make_layer(256, layers[2], stride=1, dilation=2, norm_layer=norm_layer, bn_mom=bn_mom)
+            self.layer4 = self._make_layer(512, layers[3], stride=1, dilation=4, norm_layer=norm_layer, bn_mom=bn_mom,
+                                           multi_grid=True)
+        else:
+            self.layer3 = self._make_layer(256, layers[2], stride=2, dilation=1, norm_layer=norm_layer, bn_mom=bn_mom)
+            self.layer4 = self._make_layer(512, layers[3], stride=1, dilation=2, norm_layer=norm_layer, bn_mom=bn_mom,
+                                           multi_grid=True)
 
-        current_stride = self.stem.stride
-        self._out_feature_strides = {"stem": current_stride}
-        self._out_feature_channels = {"stem": self.stem.out_channels}
+        self._out_feature_strides = {"res5": stride}
+        self._out_feature_channels = {"res5": self.layer4.out_channels}
+        self._out_features = ["res5"]
 
-        self.stages_and_names = []
-        for i, blocks in enumerate(stages):
-            for block in blocks:
-                assert isinstance(block, ResNetBlockBase), block
-                curr_channels = block.out_channels
-            stage = nn.Sequential(*blocks)
-            name = "res" + str(i + 2)
-            self.add_module(name, stage)
-            self.stages_and_names.append((stage, name))
-            self._out_feature_strides[name] = current_stride = int(
-                current_stride * np.prod([k.stride for k in blocks])
-            )
-            self._out_feature_channels[name] = blocks[-1].out_channels
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                weight_init.c2_msra_fill(m)
 
-        if num_classes is not None:
-            self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-            self.linear = nn.Linear(curr_channels, num_classes)
-            nn.init.normal_(self.linear.weight, stddev=0.01)
-            name = "linear"
+    def _make_layer(self, planes, blocks, stride=1, dilation=1, norm_layer='BN', bn_mom=0.1, multi_grid=False):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * Bottleneck.expansion:
+            downsample = nn.Sequential(
+                Conv2d(self.inplanes, planes * Bottleneck.expansion,
+                       kernel_size=1, stride=stride, bias=False),
+                get_norm(norm_layer, planes * Bottleneck.expansion, bn_mom))
 
-        if out_features is None:
-            out_features = [name]
-        self._out_features = out_features
-        assert len(self._out_features)
-        children = [x[0] for x in self.named_children()]
-        for out_feature in self._out_features:
-            assert out_feature in children, "Available children: {}".format(", ".join(children))
+        layers = []
+        multi_dilations = [1, 2, 4]
+        if multi_grid:
+            layers.append(
+                Bottleneck(self.inplanes, planes, stride, dilation=multi_dilations[0] * dilation, downsample=downsample,
+                           norm_layer=norm_layer, bn_mom=bn_mom))
+        elif dilation == 1 or dilation == 2:
+            layers.append(
+                Bottleneck(self.inplanes, planes, stride, dilation=1, downsample=downsample, norm_layer=norm_layer,
+                           bn_mom=bn_mom))
+        elif dilation == 4:
+            layers.append(
+                Bottleneck(self.inplanes, planes, stride, dilation=2, downsample=downsample, norm_layer=norm_layer,
+                           bn_mom=bn_mom))
+        else:
+            raise RuntimeError("=> unknown dilation size: {}".format(dilation))
+
+        self.inplanes = planes * Bottleneck.expansion
+        for i in range(1, blocks):
+            if multi_grid:
+                layers.append(
+                    Bottleneck(self.inplanes, planes, dilation=multi_dilations[i] * dilation, norm_layer=norm_layer,
+                               bn_mom=bn_mom))
+            else:
+                layers.append(
+                    Bottleneck(self.inplanes, planes, dilation=dilation, norm_layer=norm_layer, bn_mom=bn_mom))
+
+        return nn.Sequential(*layers)
 
     def forward(self, x):
-        outputs = {}
-        x = self.stem(x)
-        if "stem" in self._out_features:
-            outputs["stem"] = x
-        for stage, name in self.stages_and_names:
-            x = stage(x)
-            if name in self._out_features:
-                outputs[name] = x
-        if self.num_classes is not None:
-            x = self.avgpool(x)
-            x = self.linear(x)
-            if "linear" in self._out_features:
-                outputs["linear"] = x
-        return outputs
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
 
-    def output_shape(self):
-        return {
-            name: ShapeSpec(
-                channels=self._out_feature_channels[name], stride=self._out_feature_strides[name]
-            )
-            for name in self._out_features
-        }
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        return {'res5': x}
 
 
 @BACKBONE_REGISTRY.register()
@@ -219,26 +165,5 @@ def build_dilated_resnet_backbone(cfg, input_shape):
     bn_mom = cfg.MODEL.EMA.BN_MOM
     # fmt: on
 
-    stem = BasicStem(in_channels=input_shape.channels, out_channels=128, norm=norm, bn_mom=bn_mom)
-
     num_blocks_per_stage = {50: [3, 4, 6, 3], 101: [3, 4, 23, 3], 152: [3, 8, 36, 3]}[depth]
-
-    stages = []
-    layer1 = make_stage(128, 64, num_blocks_per_stage[0], norm=norm, bn_mom=bn_mom)
-    layer2 = make_stage(64 * 4, 128, num_blocks_per_stage[1], stride=2, norm=norm, bn_mom=bn_mom)
-
-    if stride == 16:
-        layer3 = make_stage(128 * 4, 256, num_blocks_per_stage[2], stride=2, norm=norm, bn_mom=bn_mom)
-        layer4 = make_stage(256 * 4, 512, num_blocks_per_stage[3], stride=1, norm=norm, bn_mom=bn_mom, dilation=2,
-                            grids=[1, 2, 4])
-    elif stride == 8:
-        layer3 = make_stage(128 * 4, 256, num_blocks_per_stage[2], stride=1, norm=norm, bn_mom=bn_mom, dilation=2)
-        layer4 = make_stage(256 * 4, 512, num_blocks_per_stage[3], stride=1, norm=norm, bn_mom=bn_mom, dilation=4,
-                            grids=[1, 2, 4])
-    else:
-        raise RuntimeError('=> unknown stride size: {}'.format(stride))
-    stages.append(layer1)
-    stages.append(layer2)
-    stages.append(layer3)
-    stages.append(layer4)
-    return DilatedResNet(stem, stages)
+    return DilatedResNet(num_blocks_per_stage, stride, norm, bn_mom)
